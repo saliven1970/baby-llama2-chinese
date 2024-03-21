3cc0315ebbc4095fbd394450f2b6923ceebb240f
saliven1970 <37821954+saliven1970@users.noreply.github.com>
2024-03-21 15:38:32 +0800

    修改数据存储目录，增加注释

--- sft_data_process.py
+++ sft_data_process.py
@@ -1,51 +1,54 @@
-import json
-import numpy as np
+import json,os
 from tqdm import tqdm
 import pandas as pd
 
 
 def sft_process():
-    with open('./sft_data/alpaca_gpt4_data_zh.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    #
     q_lst = []
     a_lst = []
-    for per in data:
-        q = per['instruction']
-        i = per['input']
-        a = per['output']
-        q = q + i
-        if len(q) < 10 or len(a) < 5:
-            continue
-        if len(q) > 256 or len(a) > 256:
-            continue
-        q_lst.append(q)
-        a_lst.append(a)
+    # 这是json文件的处理方法，整个文件是一个json对象
+    data_path_list = ['./finetune_data/alpaca_gpt4_data_zh.json']
+    for data_path in tqdm(data_path_list):
+        with open(data_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)      # 一次把文件读入，并加载到json
+        for per in data:             # 然后一行行数据处理
+            q = per['instruction']
+            i = per['input']
+            a = per['output']
+            q = q + i
+            if len(q) < 10 or len(a) < 5:
+                continue
+            if len(q) > 512 or len(a) > 512:
+                continue
+            q_lst.append(q)
+            a_lst.append(a)
 
-    f = open('./sft_data/Belle_open_source_1M.json', 'r', encoding='utf-8')
-
-    # s
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        per = json.loads(line)
-        q = per['instruction']
-        i = per['input']
-        a = per['output']
-        q = q + i
-        if len(q) < 10 or len(a) < 5:
-            continue
-        if len(q) > 256 or len(a) > 256:
-            continue
-        q_lst.append(q)
-        a_lst.append(a)
+    # 这是jsonl文件的处理方法，一行一个json对象
+    data_path_list = ['./finetune_data/Belle_open_source_1M.json']
+    for data_path in tqdm(data_path_list):        
+        f = open(data_path, 'r', encoding='utf-8')
+        while True:
+            line = f.readline()    # 一行一行读入文件
+            if not line:
+                break
+            per = json.loads(line) # 加载到json
+            q = per['instruction']
+            i = per['input']
+            a = per['output']
+            q = q + i
+            if len(q) < 10 or len(a) < 5:
+                continue
+            if len(q) > 512 or len(a) > 512:
+                continue
+            q_lst.append(q)
+            a_lst.append(a)
+            
     df = pd.DataFrame(columns=['prompt', 'answer'])
     df['prompt'] = q_lst
     df['answer'] = a_lst
-    df.to_csv('sft_data/sft_data.csv', index=False)
-    print(df)
+    df.to_csv('./finetune_data/sft_data.csv', index=False)
 
-save_dir = './sft_data'
-if not os.path.exists(save_dir): os.makedirs(save_dir)
-sft_process()
+if __name__=="__main__":
+    save_dir = './finetune_data'
+    if not os.path.exists(save_dir): os.makedirs(save_dir)
+    sft_process()
